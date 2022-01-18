56ef22c743d46fa892ed6f03fd075079d8648104
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-18 09:43:04 -0500

    initial file structure and beginning styling

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Function to create welcome screen with title, instructions and start button
+
+// Create Game play screen with question answer choices, and cycle through questions
+
+// Check answer, keep score, update timer and display feedback
+
+// Timer function
+
+// End game, display score, check high scores
+
+// Update high score in local storage
+
+// Display high scores
+
+// ***Optional*** randomize function to set the question order and possible answer order
