b41a9186a4c2a85a90c1920d334454c2bcadc612
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-22 23:00:13 -0500

    finished the score save feature and provided additional styling

--- assets/js/highscorescript.js
+++ assets/js/highscorescript.js
@@ -1,21 +1,53 @@
 var submitHighScoreEl = document.querySelector("#save-initials");
+var clearHighScoreEl = document.querySelector("#clear-high-scores");
+var playerScore = JSON.parse(localStorage.getItem("score"));
 
-var logHighScore = function() {
+var checkHighScore = function() {
     event.preventDefault();
     var initialsInput = document.querySelector("#initials-input").value;
-    console.log(initialsInput);
     var hideEl = document.querySelector("#display-score");
     hideEl.setAttribute ("style", "display: none;");
+    // Get the high score array from local storage and add recent score to array
+    var highScore = {
+        initials: initialsInput,
+        score: playerScore
+    };
+    var previousHighScore = JSON.parse(localStorage.getItem("codeQuizHighScores"));
+
+
+    // localStorage.setItem("codeQuizHighScores", JSON.stringify(highScore))
+    
+    if (!previousHighScore) {
+        console.log("no High Scores");
+        localStorage.setItem("codeQuizHighScores", JSON.stringify(highScore))
+    }
+    else if (highScore.score > previousHighScore.score) {
+        console.log(previousHighScore.score);
+        console.log(highScore.score)
+        localStorage.setItem("codeQuizHighScores", JSON.stringify(highScore));
+    };
+    var highScoreContainerEl = document.querySelector("#high-score-container");
+    highScoreContainerEl.setAttribute("style","display:flex;");
+    var listContainerEl = document.querySelector("#high-score-list")
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "high-score-item";
+    var scoreDisplay = JSON.parse(localStorage.getItem("codeQuizHighScores"));
+    listItemEl.innerHTML = "Score:  " + scoreDisplay.score + "     ------  Player: " +scoreDisplay.initials;
+    listContainerEl.appendChild(listItemEl);
 };
 
-var displayHighScore = function () {
-    var score = JSON.parse(localStorage.getItem("score"));
-    console.log(score);
+var displayPlayerScore = function () {
+    // console.log(playerScore);
     var scoreDisplayEl = document.querySelector("#report-score");
-    scoreDisplayEl.innerHTML = "Great work! Your score is " + score + "!";
+    if (playerScore < 0) {
+        playerScore = 0;
+    }
+    else {
+    scoreDisplayEl.innerHTML =  "Great work! Your score is " + playerScore + "!";
+    };
 };
-var fillHighScoreBoard = function() {
-
-};
-submitHighScoreEl.addEventListener("click", logHighScore);
-displayHighScore();
+submitHighScoreEl.addEventListener("click", checkHighScore);
+clearHighScoreEl.addEventListener("click", function() {
+    localStorage.removeItem("codeQuizHighScores");
+});
+displayPlayerScore();
